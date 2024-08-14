1caee08c90e4b30d2238bd9262749f5b20abbc8d
Dag Andreas <daanfofo@gmail.com>
2024-08-14 13:16:54 +0200

    addbagel base works. Refactoring addBagel.

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,14 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
+    //public static HashMap<String, Intege>
+    static int maxBasketSize = 5; // default value
+    private ArrayList<String> basket = new ArrayList<>();
+
+    public boolean addBagel(String type){
+        return true;
+    }
 
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -14,6 +14,6 @@ class BasketTest {
     @Test
     void testAddBagel(){
         Basket basket = new Basket();
-        basket.add("Whole wheat");
+        Assertions.assertTrue(basket.addBagel("Whole wheat"));
     }
 }
